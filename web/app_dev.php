6c750345b9b72ca2b8261cd71d75f5e320c6a9c3
abula <abula@216ea19e-b572-44bf-bedd-0b0ba5757e72>
2012-10-30 20:10:30 +0000

    feature: basic database config etc
    git-svn-id: http://svn.pulu.org/palsta/trunk@9 216ea19e-b572-44bf-bedd-0b0ba5757e72

--- web/app_dev.php
+++ web/app_dev.php
@@ -8,16 +8,22 @@ umask(0000);
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
-if (isset($_SERVER['HTTP_CLIENT_IP'])
-    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !in_array(@$_SERVER['REMOTE_ADDR'], array(
-        '127.0.0.1',
-	'89.166.113.9',
-        '::1',
-    ))
-) {
-    header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+$allowed_ips = array(
+    '127.0.0.1',
+    '89.166.105.71',
+    '::1');
+$username = 'admin';
+$password = 'kurppa6';
+if (isset($_SERVER['HTTP_CLIENT_IP']) || isset($_SERVER['HTTP_X_FORWARDED_FOR']) || ! in_array(@$_SERVER['REMOTE_ADDR'], $allowed_ips)) {
+    if ($_SERVER['PHP_AUTH_PW'] == $password && $_SERVER['PHP_AUTH_USER'] == $username) {
+    } else {
+        header('WWW-Authenticate: Basic realm="Pulupalsta Authentication"');
+        header('HTTP/1.0 401 Unauthorized');
+        echo "You must enter a valid login ID and password to access this resource\n";
+        exit;
+    }
+    //header('HTTP/1.0 403 Forbidden');
+    //exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
