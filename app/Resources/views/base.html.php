e93795a88f4656ead35d2d3aa63bb44cdec6869a
abula <abula@216ea19e-b572-44bf-bedd-0b0ba5757e72>
2014-01-09 19:15:30 +0000

    feature: google analytics code
    git-svn-id: http://svn.pulu.org/palsta/trunk@94 216ea19e-b572-44bf-bedd-0b0ba5757e72

--- app/Resources/views/base.html.php
+++ app/Resources/views/base.html.php
@@ -162,7 +162,7 @@ var translations = {
 <script src="javascripts/app.js"></script>-->*/ ?>
 
 <script type="text/javascript">
-var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+var _gaq=[['_setAccount','UA-10351274-1'],['_trackPageview']];
 (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
 g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
 s.parentNode.insertBefore(g,s)}(document,'script'));
