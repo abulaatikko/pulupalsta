78a8ffaac61226f6d10efe43cb9840fd32839ee7
Lassi Heikkinen <lassi@pulu.org>
2015-11-17 21:15:44 +0000

    change: app_dev auth is done via nginx confs

--- web/app_dev.php
+++ web/app_dev.php
@@ -6,29 +6,6 @@ use Symfony\Component\HttpFoundation\Request;
 // read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
 umask(0000);
 
-// This check prevents access to debug front controllers that are deployed by accident to production servers.
-// Feel free to remove this, extend it, or make something more sophisticated.
-$allowed_ips = array(
-    '127.0.0.1',
-    '89.166.105.71',
-    '::1');
-$username = 'admin';
-$password = 'KurppaKuusiSeittemänJaKaheksan';
-$ok = false;
-if ($_SERVER['PHP_AUTH_PW'] == $password && $_SERVER['PHP_AUTH_USER'] == $username) {
-    $ok = true;
-} else {
-    header('WWW-Authenticate: Basic realm="Pulupalsta Authentication"');
-    header('HTTP/1.0 401 Unauthorized');
-    echo "You must enter a valid login ID and password to access this resource\n";
-    exit;
-}
-
-if (! $ok) {
-    header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
-}
-
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
 
