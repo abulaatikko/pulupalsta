29900e24232e186d11f0e08c96f998aaded6ab3d
Lassi Heikkinen <lassi@pulu.org>
2015-11-27 17:56:55 +0000

    bugfix: Index doesnt show keywords in abc order in English

--- src/Pulu/PalstaBundle/Controller/IndexController.php
+++ src/Pulu/PalstaBundle/Controller/IndexController.php
@@ -4,11 +4,13 @@ namespace Pulu\PalstaBundle\Controller;
 use Pulu\PalstaBundle\Entity\Article;
 use Pulu\PalstaBundle\Entity\ArticleLocalization;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class IndexController extends Controller {
 
-    public function indexAction() {
+    public function indexAction(Request $R) {
         $keywordRepository = $this->getDoctrine()->getRepository('PuluPalstaBundle:Keyword');
+        $keywordRepository->setLanguage($R->getLocale());
         $keywords = $keywordRepository->findAllOrderedByName();
 
         return $this->render('PuluPalstaBundle:Index:index.html.php', array(
