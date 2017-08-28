2224c3069d6f3c66a7001c7063c373be0419aee7
Lassi Heikkinen <lassi@pulu.org>
2017-08-28 18:23:10 +0000

    Reserve image dimensions for lazy-load images

--- src/Pulu/PalstaBundle/Resources/views/Article/view.html.php
+++ src/Pulu/PalstaBundle/Resources/views/Article/view.html.php
@@ -204,6 +204,12 @@ function displayImage($filename, $width = null, $height = null, $caption = "", $
     if (is_null($alt)) {
         $alt = mb_substr($desc, 40);
     }
+    if (!empty($dimensions[0])) {
+        $out .= ' width="' . $dimensions[0] . '" ';
+    }
+    if (!empty($dimensions[1])) {
+        $out .= ' height="' . $dimensions[1] . '" ';
+    }
     $out .= ' class="js-lazy-image" data-src="' . $mediaPath . $display_url . '" alt="' . $alt . '" /></a>';
     if (! $is_thumb) {
         $out .= '<p>' . $captionWithoutHtml . '</p>';
