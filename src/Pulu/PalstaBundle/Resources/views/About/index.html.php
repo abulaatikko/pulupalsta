c9cd41efa0e03095ee0935e085a79397de7fcaaa
Lassi Heikkinen <lassi@pulu.org>
2019-05-13 04:02:53 +0000

    Update Front texts

--- src/Pulu/PalstaBundle/Resources/views/About/index.html.php
+++ src/Pulu/PalstaBundle/Resources/views/About/index.html.php
@@ -4,11 +4,11 @@
 
 <?php $view['slots']->start('body') ?>
 
-<h1>Yes?</h1>
+<h1>?</h1>
 
-<p>Pulupalsta is a playground project to practice web development and writing skills.</p>
+<p>Pulupalsta is a playground project to practice web development, writing and life in general. Also personal memories and some fun.</p>
 
-<p>The first article (<a href="http://palsta.pulu.org/fi/1-1500-litraa-pepsiC3A4-ja-sony-vaio-s5">1500 litres of Pepsi and Sony Vaio S5</a>) was written in January 2006. The Pepsi project was a primary reason to create this web site, titled <em>Puluprojects</em> back then. The platform was upgraded to Symfony PHP framework in 2012 and a fancy administration portal was created. See <a href="<?php echo $view['assets']->getUrl('bundles/pulupalsta/text/CHANGELOG.txt') ?>">CHANGELOG.txt</a> for more history details.</p>
+<p>The first article (<a href="http://palsta.pulu.org/fi/1-1500-litraa-pepsiC3A4-ja-sony-vaio-s5">1500 litres of Pepsi and Sony Vaio S5</a>) was written in January 2006. The Pepsi project was a primary reason to create this web site, titled <em>Puluprojects</em> back then. The site was heavily updated in 2012 and 2019. See <a href="<?php echo $view['assets']->getUrl('bundles/pulupalsta/text/CHANGELOG.txt') ?>">CHANGELOG.txt</a> for more history details.</p>
 
 <?php
 $tz  = new DateTimeZone('Europe/Helsinki');
--- src/Pulu/PalstaBundle/Resources/views/Front/index.html.php
+++ src/Pulu/PalstaBundle/Resources/views/Front/index.html.php
@@ -8,13 +8,38 @@
 
 <div id="locale" data-locale="<?php echo $currentLocale ?>"></div>
 
-<h1>A user of internet, welcome! <a style="float: right" href="<?php echo $view['router']->path('pulu_palsta_list') ?>#feeds" title="RSS Feeds"><img src="<?php echo $view['assets']->getUrl('bundles/pulupalsta/images/icons/32_feed.png') ?>" alt="RSS Feeds" /></a></h1>
+<h1>Welcome, a user of internet <a style="float: right" href="<?php echo $view['router']->path('pulu_palsta_list') ?>#feeds" title="RSS Feeds"><img src="<?php echo $view['assets']->getUrl('bundles/pulupalsta/images/icons/32_feed.png') ?>" alt="RSS Feeds" /></a></h1>
 
-<p>Hi, I'm Lassi in real life and Abula in internet life. I'm a web developer and this is my playground project. I've also created some content for the universe. The top-10 most popular articles are in <strong>bold</strong>.</p>
+<p>Hi, I'm Lassi in real life and Abula in internet life. I'm a web developer and this is my playground. I've also created some content for the universe. Most texts are in Finnish. The top-10 most popular articles are in <strong>bold</strong>. A <a href="/en/71-guestbook-vieraskirja">guestbook</a> exists.</p>
 
 <!-- Popular/Recent articles -->
 <div class="row">
     <div class="six columns" id="visited-articles">
+
+<h3>Expeditions</h3>
+
+<table class="wide">
+<thead>
+<tr>
+    <th>Published</th>
+    <th colspan="2">Report</th>
+</tr>
+</thead>
+
+<tbody>
+<?php foreach ($expeditionArticles as $article): ?>
+<tr>
+    <td><?php echo $article->getPublished()->format('Y-m-d') ?></td>
+    <td><a href='<?php echo $view['router']->path('pulu_palsta_article', array('article_number' => $article->getArticleNumber(), 'name' => $view['helper']->toFilename($article->getName()), '_locale' => $article->getLanguage())) ?>'><?php echo $article->getIsOneOfBest() ? '<strong>' : '' ?><?php echo $article->getName(); ?><?php echo $article->getIsOneOfBest() ? '</strong>' : '' ?></a></td>
+    <td class="centered" style="width: 30px"> <img class="flag" src="<?php echo $view['assets']->getUrl('bundles/pulupalsta/images/icons/' . $article->getLanguage() . '.svg') ?>" alt="" /></td>
+</tr>
+<?php endforeach ?>
+</tbody>
+</table>
+<p></p>
+
+    </div>
+    <div class="six columns" id="recent-articles">
 
 <h3>Research</h3>
 
@@ -35,32 +60,6 @@
 <?php endforeach ?>
 </tbody>
 </table>
-
-    </div>
-    <div class="six columns" id="recent-articles">
-
-<h3>Expeditions</h3>
-
-<table class="wide">
-<thead>
-<tr>
-    <th>Published</th>
-    <th colspan="2">Training</th>
-</tr>
-</thead>
-
-<tbody>
-<?php foreach ($expeditionArticles as $article): ?>
-<tr>
-    <td><?php echo $article->getPublished()->format('Y-m-d') ?></td>
-    <td><a href='<?php echo $view['router']->path('pulu_palsta_article', array('article_number' => $article->getArticleNumber(), 'name' => $view['helper']->toFilename($article->getName()), '_locale' => $article->getLanguage())) ?>'><?php echo $article->getIsOneOfBest() ? '<strong>' : '' ?><?php echo $article->getName(); ?><?php echo $article->getIsOneOfBest() ? '</strong>' : '' ?></a></td>
-    <td class="centered" style="width: 30px"> <img class="flag" src="<?php echo $view['assets']->getUrl('bundles/pulupalsta/images/icons/' . $article->getLanguage() . '.svg') ?>" alt="" /></td>
-</tr>
-<?php endforeach ?>
-</tbody>
-</table>
-
-<p></p>
 
     </div>
 </div><!-- Popular/Recent articles ends -->
