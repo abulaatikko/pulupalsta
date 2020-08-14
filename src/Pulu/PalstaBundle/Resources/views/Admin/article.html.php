5fa063f1eb80a60656a31d4e3ecf7dbbfd731532
Lassi Heikkinen <lassi@pulu.org>
2020-08-14 12:46:08 +0000

    Decrease opacity for opinion category articles in admin to hide them

--- src/Pulu/PalstaBundle/Resources/views/Admin/article.html.php
+++ src/Pulu/PalstaBundle/Resources/views/Admin/article.html.php
@@ -17,21 +17,24 @@
     <th colspan="3">Kirjoitus</th>
     <th>Kirjoitettu</th>
     <th>Muokattu</th>
+    <th>Commentable</th>
 </tr>
 </thead>
 
 <tbody>
 <?php foreach ($articles as $article): ?>
-<?php $isPublicStyle = $article->isPublic() ? '' : ' style="text-decoration: line-through"'; ?>
+<?php $isPublicStyle = $article->isPublic() ? '' : ' text-decoration: line-through;'; ?>
+<?php $isOpinionStyle = $article->getType() !== 4 ? '' : ' display: initial; opacity: 20%;'; ?>
 <?php $typeText = $articleTypes[$article->getType()] ?? '' ?>
 <?php $typeStyles = 'font-size: 60%; text-align: right; font-weight: bold; color: ' . $article->getTypeColor(); ?>
 <tr>
-    <td<?php echo $isPublicStyle ?>><a href="<?php echo $view['router']->path('pulu_palsta_admin_article_edit', array('id' => $article->getId())) ?>"><?php echo $article->getIsOneOfBest() ? '<strong>' : '' ?><?php echo $article->getName(); ?><?php echo $article->getIsOneOfBest() ? '</strong>' : '' ?></a> (<?php echo $article->getArticleNumber() ?>)</td>
+    <td style="<?php echo $isPublicStyle ?>"><span style="<?php echo $isOpinionStyle ?>"><a href="<?php echo $view['router']->path('pulu_palsta_admin_article_edit', array('id' => $article->getId())) ?>"><?php echo $article->getIsOneOfBest() ? '<strong>' : '' ?><?php echo $article->getName(); ?><?php echo $article->getIsOneOfBest() ? '</strong>' : '' ?></a> (<?php echo $article->getArticleNumber() ?>)</span></td>
     <td class="centered" style="width: 30px"> <img class="flag" src="<?php echo $view['assets']->getUrl('bundles/pulupalsta/images/icons/' . $article->getLanguage() . '.svg') ?>" alt="" /></td>
     <td style="<?php echo $typeStyles; ?>"><?php echo $article->getTypeName() ?></td>
 
     <td class="nowrap"><?php echo $article->getCreated()->format('Y-m-d') ?></td>
     <td class="nowrap"><?php echo $article->getModified()->format('Y-m-d') ?></td>
+    <td class="centered"><?php echo $article->getIsCommentable() ? 'X' : '' ?></td>
 </tr>
 <?php endforeach ?>
 
