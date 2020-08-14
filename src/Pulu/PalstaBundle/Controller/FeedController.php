6bed5d05fc515583de544c72225838899c7c6bff
Lassi Heikkinen <lassi@pulu.org>
2020-08-14 12:52:35 +0000

    Remove recentArticles and recentComments RSS feeds

--- src/Pulu/PalstaBundle/Controller/FeedController.php
+++ src/Pulu/PalstaBundle/Controller/FeedController.php
@@ -6,18 +6,6 @@ use Symfony\Component\HttpFoundation\Request;
 
 class FeedController extends Controller {
 
-    public function recentArticlesAction(Request $R) {
-        $locale = $R->getLocale();
-        $repository = $this->getDoctrine()->getRepository('PuluPalstaBundle:Article');
-        $repository->setLanguage($locale);
-        $recentArticles = $repository->findOrderedByPublishedForPublic(10);        
-
-        return $this->render('PuluPalstaBundle:Feed:recentArticles.xml.php', array(
-            'recentArticles' => $recentArticles,
-            'locale' => $locale
-        ));
-    }
-
     public function articlesAction(Request $R) {
         $locale = $R->getLocale();
         $repository = $this->getDoctrine()->getRepository('PuluPalstaBundle:Article');
@@ -25,17 +13,6 @@ class FeedController extends Controller {
 
         return $this->render('PuluPalstaBundle:Feed:articles.xml.php', array(
             'articles' => $articles
-        ));
-    }
-
-    public function recentCommentsAction(Request $R) {
-        $locale = $R->getLocale();
-        $repository = $this->getDoctrine()->getRepository('PuluPalstaBundle:Comment');
-        $recentComments = $repository->findByCreated(10, null, $locale);        
-
-        return $this->render('PuluPalstaBundle:Feed:recentComments.xml.php', array(
-            'recentComments' => $recentComments,
-            'locale' => $locale
         ));
     }
 
