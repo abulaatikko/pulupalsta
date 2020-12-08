b928e79dc4109fc58cb17a4e2e3750bb91d80eea
Lassi Heikkinen <lassi@pulu.org>
2020-12-08 13:29:15 +0000

    Improve RSS feed

--- src/Pulu/PalstaBundle/Resources/views/Feed/articles.xml.php
+++ src/Pulu/PalstaBundle/Resources/views/Feed/articles.xml.php
@@ -1,10 +1,10 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8" ?>' . "\n"; ?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
 <channel>
-    <title>Palsta Articles</title>
+    <title>Pulu-Z</title>
     <description>Random projects</description>
-    <link>https://palsta.pulu.org</link>
-    <atom:link href="https://palsta.pulu.org/en/feed/articles" rel="self" type="application/rss+xml" />
+    <link>https://z.pulu.org</link>
+    <atom:link href="https://z.pulu.org/en/feed/articles" rel="self" type="application/rss+xml" />
     <lastBuildDate><?php echo date('D, d M Y H:i:s O'); ?></lastBuildDate>
     <pubDate><?php echo date('D, d M Y H:i:s O'); ?></pubDate>
 
@@ -12,6 +12,7 @@
     <item>
         <title><?php echo $article->getName() ?> (#<?php echo $article->getArticleNumber() ?>)</title>
         <link>https:<?php echo $view['router']->url('pulu_palsta_article', array('_locale' => $article->getLanguage(), 'article_number' => $article->getArticleNumber(), 'name' => $view['helper']->toFilename($article->getName())), true) ?></link>
+        <description><?php echo $article->getTeaser() ?></description>
         <guid>https:<?php echo $view['router']->url('pulu_palsta_article', array('_locale' => $article->getLanguage(), 'article_number' => $article->getArticleNumber(), 'name' => $view['helper']->toFilename($article->getName())), true) ?></guid>
         <pubDate><?php echo $article->getPublished()->format('D, d M Y H:i:s O') ?></pubDate>
     </item>
